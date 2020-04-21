9b3ea97aec7e4f3d5ac97815c85e16f87fce9ce1
Jeff Gueldre <jfgueldre@hotmail.com>
2020-04-21 22:29:38 +0200

    Revert "Revert ".""
    This reverts commit 33387eb7ecfd906627c09460fa6075741e273d9a.

--- CoreForm/wwwroot/builder/js/script_common.js
+++ CoreForm/wwwroot/builder/js/script_common.js
@@ -26,9 +26,10 @@ var formValidators = {
 function RegisterField(fieldDefinition) {
 
     if (!fieldDefinition.fieldTemplate.computed) fieldDefinition.fieldTemplate.computed = {};
+    if (!fieldDefinition.editForm.computed) fieldDefinition.editForm.computed = {};
     if (!fieldDefinition.fieldTemplate.methods) fieldDefinition.fieldTemplate.methods = {};
     fieldDefinition.fieldTemplate.computed.$validation = function () {
-        return this.schema.variable ? (app.$v.data[this.schema.variable] ? app.$v.data[this.schema.variable] : null) : null;
+        return this.schema.variable ? (this.$root.$v.data[this.schema.variable] ? this.$root.$v.data[this.schema.variable] : null) : null;
     }
 
     fieldDefinition.fieldTemplate.computed.$isrequired = function () {
@@ -39,6 +40,8 @@ function RegisterField(fieldDefinition) {
         }
     }
 
+
+
     fieldDefinition.fieldTemplate.computed.$error = function () {
         return (this.$validation ? this.$validation.$error : false);
     }
@@ -47,8 +50,8 @@ function RegisterField(fieldDefinition) {
         if (this.$validation && this.$validation.$error && this.schema.variable) {
             for (const valid in this.$validation) {
                 if (!String(this.$validation[valid]).startsWith("$") && this.$validation[valid] === false) {
-                    for (const varid in app.schema.variables) {
-                        var variable = app.schema.variables[varid];
+                    for (const varid in this.$root.schema.variables) {
+                        var variable = this.$root.schema.variables[varid];
                         if (variable.name === this.schema.variable) {
                             for (const vali in variable.validations) {
                                 var validation = variable.validations[vali];
@@ -65,82 +68,16 @@ function RegisterField(fieldDefinition) {
         return "";
     }
 
-
     fieldDefinition.fieldTemplate.methods.$touch = function () {
         (this.$validation ? this.$validation.$touch() : false);
     };
 
+    fieldDefinition.editForm.computed.$validation = function () {
+        return this.$root.$v.editformdata;
+    }
+
+
     registeredFields.set(fieldDefinition.type, fieldDefinition);
 }
-
-
-
-
-//traverse nodes and sub-nodes ans execute function for each node found
-function tranverseDataNodes(node, func) {
-    var subColl = null;
-    if (typeof (node.columns) !== "undefined") subColl = node.columns;
-    if (typeof (node.fields) !== "undefined") subColl = node.fields;
-    func(node);
-    if (subColl !== null) {
-        for (var i = 0; i < subColl.length; i++) {
-            tranverseDataNodes(subColl[i], func);
-        }
-    }
-}
-
-function findSchemaObjectById(id) {
-    if (id === "formContainer") return app.schema;
-    return findDataObjectByDataNode(app.schema, id)
-}
-
-function findDataObjectByDataNode(node, id) {
-    var subColl = null;
-    if (typeof (node.columns) !== "undefined") subColl = node.columns;
-    if (typeof (node.fields) !== "undefined") subColl = node.fields;
-
-    if (id === node.id) {
-        return node;
-    } else {
-        if (subColl !== null) {
-            for (var i = 0; i < subColl.length; i++) {
-                var res = findDataObjectByDataNode(subColl[i], id);
-                if (res !== null) return res;
-            }
-        }
-    }
-    return null;
-}
-
-function findDataCollectionByElement(element) {
-    var id = null;
-    if ($(element).attr("id")) id = $(element).attr("id");
-    else if ($(element).data("ref")) id = $(element).data("ref");
-
-    if (id === "formContainer") return app.schema.fields;
-    return findDataCollectionInDataNode(app.schema, id)
-}
-
-function findDataCollectionInDataNode(node, id) {
-    var subColl = null;
-    if (typeof (node.columns) !== "undefined") subColl = node.columns;
-    if (typeof (node.fields) !== "undefined") subColl = node.fields;
-
-    if (subColl === null) return null;
-
-    if (id === node.id) {
-        return subColl;
-    } else {
-        for (var i = 0; i < subColl.length; i++) {
-            var res = findDataCollectionInDataNode(subColl[i], id);
-            if (res !== null) return res;
-        }
-    }
-    return null;
-}
-
-
-
-
 
 
